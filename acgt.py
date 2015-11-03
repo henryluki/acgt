a092bcaf4316e93023ec5ef0297fd450837ddab6
henryluki <iamamiracle55@gmail.com>
2015-11-03 11:59:01 +0800

    take arguments from command line done

--- acgt.py
+++ acgt.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-import io,json
+import io,json,sys,getopt
 from gen import Gen
 
 class Acgt(object):
@@ -37,6 +37,41 @@ class Acgt(object):
   def parse_detail(self, arg):
     return dict({'arguments':arg["arguments"], 'method':arg["method"], 'name':arg["name"], 'results': arg["results"]})
 
+  @classmethod
+  def take_arguments(self):
+    if sys.argv[1:] == []:
+      self.usage()
+    try:
+      opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "api="])
+    except getopt.GetoptError as err:
+      print str(err)
+      self.usage()
+      sys.exit(2)
+    self.execute_feature(opts)
+
+  @classmethod
+  def execute_feature(self, opts):
+    for o, a in opts:
+      if o == "-h" or o == "--help":
+        self.usage()
+        sys.exit(2)
+      elif o == "--api":
+        if a == "flask":
+          print "Generate app..."
+          self.parse_apis()
+          print "Done!"
+        elif a == "js":
+          print "js..."
+        else:
+          print "Take some feature liki 'flask'"
+      else:
+        assert False, "Option error"
+
+  @classmethod
+  def usage(self):
+    print "*** usage info ***"
+    print "--api=(flask,js)   generate code by acgt"
+    print "-h  --help         usage info "
 
 if __name__ == '__main__':
-  Acgt.parse_apis()
+  Acgt.take_arguments()
